041a00003bc07d9e3fa4c8ce91a0dd85c3381ed1
fun-minion <willemjansen891003@gmail.com>
2024-12-12 04:15:47 +1100

    team profile & logo updates

--- src/pages/Team/index.jsx
+++ src/pages/Team/index.jsx
@@ -4,16 +4,16 @@ const Team = () => {
     const people = [
         {
             img_url: '/team/dino-removed.png',
-            location: 'New York',
+            location: 'Halifax',
             name: 'Dino Mariutti',
-            title: ['Partner', 'Broad Member Executive Chairman'],
+            title: ['Partner', 'Board Member Executive Chairman'],
             description: 'Dino Mariutti is the Founder and Executive Chairman of Apexmont Inc., a position he has held since founding the company in 2020. With a background in engineering and a Global MBA from University College London, Dino also holds a CPD Accredited certificate in Algorithmic Trading from Oxford’s Saïd Business School. His expertise lies in applying cutting-edge trading strategies to dynamic market environments, with a focus on driving risk-adjusted returns and capturing alpha.',
         },
         {
             img_url: '/team/thomas-removed.png',
-            location: 'New York',
+            location: 'Halifax',
             name: 'Dr. Thomas Larder',
-            title: ['Partner', 'Broad Member'],
+            title: ['Partner', 'Board Member'],
             description: 'Dr. Thomas Larder is an Advisor, Board Member and shareholder of Apexmont Inc. since 2020. Tom has a background in healthcare (Tufts University/Boston), relationship building and financial services (Investment/Insurance). He has a reputation of fairness, transparency, and accountability that fellow business people and directors have all come to admire. His main areas of interest are strategic planning, high level oversight, privacy issues and innovation.',
         },
     ]
